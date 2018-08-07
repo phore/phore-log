d6fe5d3ce183d18de7698fc6ebd9133bb55178e5
Matthias <m@tth.es>
2018-08-07 09:30:15 +0200

    moved backtrace

--- src/functions.php
+++ src/functions.php
@@ -14,8 +14,10 @@
 public function phore_log (...$params) : \Phore\Log\PhoreLog
 {
     $logger = \Phore\Log\PhoreLog::GetInstance();
-    $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
-    if (count($params) > 0)
+
+    if (count($params) > 0) {
+        $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
         $logger->log(\Phore\Log\PhoreLog::LOG_DEBUG, $backtrace[0]["file"], $backtrace[0]["line"], ...$params);
+    }
     return $logger;
 }
