a3994c1fad221fc52dcbcfedce4b80a54fb0c81e
Matthias <m@tth.es>
2018-08-07 12:22:52 +0200

    log

--- src/functions.php
+++ src/functions.php
@@ -15,7 +15,7 @@ function phore_log (...$params) : \Phore\Log\PhoreLog
 {
     $logger = \Phore\Log\PhoreLog::GetInstance();
 
-    if (count($params) > 0 && $logger->getVerbosity() < \Phore\Log\PhoreLog::LOG_DEBUG) {
+    if (count($params) > 0 && $logger->getVerbosity() <= \Phore\Log\PhoreLog::LOG_DEBUG) {
         $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
         $logger->log(\Phore\Log\PhoreLog::LOG_DEBUG, $backtrace[0]["file"], $backtrace[0]["line"], ...$params);
     }
