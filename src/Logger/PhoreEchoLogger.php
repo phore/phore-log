1e2b8e03f83d9496664239be009b291b18e42f4f
Matthias <m@tth.es>
2018-09-21 10:24:39 +0200

    set default log to stderr

--- src/Logger/PhoreEchoLogger.php
+++ src/Logger/PhoreEchoLogger.php
@@ -16,17 +16,26 @@ class PhoreEchoLogger implements PhoreLogger
 
     private $lastFile = null;
 
+    private $channel;
+    
+    public function __construct($logTo="php://stderr")
+    {
+        $this->channel = $logTo;
+    }
+
+
     public function log (int $severity, string $file, int $lineNo, ...$params)
     {
         $severityMap = [
             9 => "DEBUG",
             5 => "INFO ",
-            2 => "WARN ",
-            1 => " ERR "
+            3 => "WARN ",
+            2 => " ERR ",
+            1 => "EMGY "
         ];
 
         if ($this->lastFile !== $file) {
-            echo "> $file\n";
+            file_put_contents($this->channel,"> $file\n", FILE_APPEND);
             $this->lastFile = $file;
         }
 
@@ -35,7 +44,7 @@ class PhoreEchoLogger implements PhoreLogger
         $logLine .= "[+" . str_pad(number_format(PhoreStopWatch::GetScriptRunTime(), 3, ".", ""), 7, " ", STR_PAD_LEFT) . "]";
         $logLine .= "[:" . str_pad($lineNo, 3, " ", STR_PAD_LEFT) . "]";
         $logLine .= " " . implode(" ", $params);
-        echo $logLine ."\n";
+        file_put_contents($this->channel, $logLine ."\n", FILE_APPEND);
     }
 
 }
