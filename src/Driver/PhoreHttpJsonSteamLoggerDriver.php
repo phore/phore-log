cfc32299d2d649ca638e0119a2ba267d7938ade2
Matthias <m@tth.es>
2024-12-12 10:38:34 +0100

    sync

--- src/Driver/PhoreHttpJsonSteamLoggerDriver.php
+++ src/Driver/PhoreHttpJsonSteamLoggerDriver.php
@@ -28,6 +28,7 @@ class PhoreHttpJsonSteamLoggerDriver implements PhoreLoggerDriver
         if ($logLevel > $this->minSeverity)
             return;
 
+
         $logLine = $this->logFormat->format($logLevel, $file, $lineNo, $message, $context);
         $line = json_encode([
                 "type" => "log",
@@ -37,9 +38,26 @@ class PhoreHttpJsonSteamLoggerDriver implements PhoreLoggerDriver
                 "message" => $logLine,
                 "context" => $context
         ]);
-        echo $line . "\r\n";
-        //file_put_contents("php://output", $line . "\r\n");
+        $line .= "\n";
+
+        //echo dechex(strlen($line)) . "\r\n";
+
+        $chunk = dechex(strlen($line)) . "\r\n" . $line . "\r\n";
+        echo $chunk;
         flush();
+        //ob_end_flush();
+
+       // echo $line . "\r\n";
+        //file_put_contents("php://output", $line . "\r\n");
+        //flush();
+
+    }
+
+    public function __destruct()
+    {
+       // echo "0\r\n\r\n";
+        //ob_flush(); // for fpm context
+        //flush();
     }
 
     public function setMinSeverity(LogLevelEnum $logLevel)
